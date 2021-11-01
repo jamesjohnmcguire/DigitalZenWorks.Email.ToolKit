548c3d337b0b0042a1fc794a7dba848dfc9fa4cf
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-01 21:52:56 +0900

    Filled out program initialization.

--- DbxToPst/Program.cs
+++ DbxToPst/Program.cs
@@ -1,12 +1,125 @@
-﻿using System;
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Program.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using Common.Logging;
+using DbxToPstLibrary;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+
+[assembly: CLSCompliant(true)]
 
 namespace DbxToPst
 {
-	class Program
+	/// <summary>
+	/// Dbx to pst program class.
+	/// </summary>
+	public static class Program
 	{
-		static void Main(string[] args)
+		private static readonly ILog Log = LogManager.GetLogger(
+			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		/// <summary>
+		/// The program's main entry point.
+		/// </summary>
+		/// <param name="arguments">The arguments given to the program.</param>
+		public static void Main(string[] arguments)
 		{
-			Console.WriteLine("Hello World!");
+			try
+			{
+				LogInitialization();
+				string version = GetVersion();
+
+				Log.Info("Starting DbxToPst Version: " + version);
+
+				if (arguments != null && arguments.Length > 0)
+				{
+					Migrate.DbxToPst(arguments[0]);
+				}
+				else
+				{
+					Log.Error("Invalid arguments");
+				}
+			}
+			catch (Exception exception)
+			{
+				Log.Error(exception.ToString());
+
+				throw;
+			}
+		}
+
+		private static string GetVersion()
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+
+			AssemblyName assemblyName = assembly.GetName();
+			Version version = assemblyName.Version;
+			string assemblyVersion = version.ToString();
+
+			return assemblyVersion;
+		}
+
+		private static void LogInitialization()
+		{
+			string applicationDataDirectory = @"DigitalZenWorks\DbxToPst";
+			string baseDataDirectory = Environment.GetFolderPath(
+				Environment.SpecialFolder.ApplicationData,
+				Environment.SpecialFolderOption.Create) + @"\" +
+				applicationDataDirectory;
+
+			string logFilePath = baseDataDirectory + "\\DbxToPst.log";
+			string outputTemplate =
+				"[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] " +
+				"{Message:lj}{NewLine}{Exception}";
+
+			LoggerConfiguration configuration = new ();
+			LoggerSinkConfiguration sinkConfiguration = configuration.WriteTo;
+			sinkConfiguration.Console(LogEventLevel.Verbose, outputTemplate);
+			sinkConfiguration.File(
+				logFilePath, LogEventLevel.Verbose, outputTemplate);
+			Serilog.Log.Logger = configuration.CreateLogger();
+
+			LogManager.Adapter =
+				new Common.Logging.Serilog.SerilogFactoryAdapter();
+		}
+
+		private static void ShowHelp(string additionalMessage)
+		{
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			string location = assembly.Location;
+
+			FileVersionInfo versionInfo =
+				FileVersionInfo.GetVersionInfo(location);
+
+			string companyName = versionInfo.CompanyName;
+			string copyright = versionInfo.LegalCopyright;
+
+			AssemblyName assemblyName = assembly.GetName();
+			string name = assemblyName.Name;
+			Version version = assemblyName.Version;
+			string assemblyVersion = version.ToString();
+
+			string header = string.Format(
+				CultureInfo.CurrentCulture,
+				"{0} {1} {2} {3}",
+				name,
+				assemblyVersion,
+				copyright,
+				companyName);
+			Log.Info(header);
+
+			if (!string.IsNullOrWhiteSpace(additionalMessage))
+			{
+				Log.Info(additionalMessage);
+			}
 		}
 	}
 }
