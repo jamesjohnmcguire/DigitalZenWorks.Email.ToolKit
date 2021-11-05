a441ad82a66d7133b30c27ea4b88ffb958acfec4
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-05 15:10:53 +0900

    Getting the dbx tree format worked out with new classes DbxTreeNode and DbxNodeItem.

---
+++ DbxToPstLibrary/DbxNodeItem.cs
@@ -0,0 +1,38 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="DbxNodeItem.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbxToPstLibrary
+{
+	/// <summary>
+	/// Dbx node item class.
+	/// </summary>
+	public class DbxNodeItem
+	{
+		/// <summary>
+		/// Gets or sets the node children index.
+		/// </summary>
+		/// <value>The node children index.</value>
+		public int NodeChildrenIndex { get; set; }
+
+		/// <summary>
+		/// Gets or sets the node children count.
+		/// </summary>
+		/// <value>The node children count.</value>
+		public int NodeChildrenCount { get; set; }
+
+		/// <summary>
+		/// Gets or sets the node value.
+		/// </summary>
+		/// <value>The node value.</value>
+		public int NodeValue { get; set; }
+	}
+}
