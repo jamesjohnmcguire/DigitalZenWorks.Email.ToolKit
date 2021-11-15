542f474e536f062f3ab11518ef4672f27d838a39
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-15 20:08:18 +0900

    Add DbxMessageIndex class

---
+++ DbxToPstLibrary/DbxMessageIndex.cs
@@ -0,0 +1,22 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="DbxMessageIndex.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DbxToPstLibrary
+{
+	/// <summary>
+	/// Dbx message indx class.
+	/// </summary>
+	public class DbxMessageIndex
+	{
+		/// <summary>
+		/// Gets or sets the message id.
+		/// </summary>
+		/// <value>The message id.</value>
+		public uint MessageId { get; set; }
+	}
+}
