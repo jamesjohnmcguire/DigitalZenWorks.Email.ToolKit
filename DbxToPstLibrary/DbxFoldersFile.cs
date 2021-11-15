f96ea717c414bb685f6af9f76e306df75dce0030
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-15 19:44:16 +0900

    Add MigrateFolders and MigrateFolder methods
    These are the starting points to read into the actual message files.

--- DbxToPstLibrary/DbxFoldersFile.cs
+++ DbxToPstLibrary/DbxFoldersFile.cs
@@ -7,6 +7,7 @@
 using Common.Logging;
 using System;
 using System.Globalization;
+using System.IO;
 
 namespace DbxToPstLibrary
 {
@@ -40,7 +41,8 @@ namespace DbxToPstLibrary
 		private DbxTree tree;
 
 		/// <summary>
-		/// Initializes a new instance of the <see cref="DbxFoldersFile"/> class.
+		/// Initializes a new instance of the
+		/// <see cref="DbxFoldersFile"/> class.
 		/// </summary>
 		/// <param name="filePath">The path of the dbx file.</param>
 		public DbxFoldersFile(string filePath)
@@ -90,6 +92,70 @@ namespace DbxToPstLibrary
 						"item value[{0}] is {1}",
 						Name,
 						folderIndex.FolderFileName);
+					Log.Info(message);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Migrate folder method.
+		/// </summary>
+		/// <param name="folderName">The path of the dbx folder file.</param>
+		public static void MigrateFolder(string folderName)
+		{
+			if (!string.IsNullOrWhiteSpace(folderName))
+			{
+				string foldersPath = Path.GetDirectoryName(folderName);
+				string filePath = Path.Combine(foldersPath, folderName);
+
+				bool exists = File.Exists(filePath);
+
+				if (exists == false)
+				{
+					Log.Warn(
+						filePath + " specified in Folders.dbx not present");
+				}
+				else
+				{
+					DbxMessagesFile messagesFile = new (filePath);
+
+					DbxFileType check = messagesFile.Header.FileType;
+
+					if (check != DbxFileType.MessageFile)
+					{
+						Log.Error(filePath + " not actually a messagess file");
+
+						// Attempt to process the individual files.
+					}
+					else
+					{
+						messagesFile.ReadTree();
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Migrate folders method.
+		/// </summary>
+		public void MigrateFolders()
+		{
+			if (tree != null)
+			{
+				byte[] fileBytes = GetFileBytes();
+
+				foreach (uint index in tree.FolderInformationIndexes)
+				{
+					DbxFolderIndexedItem item = new(fileBytes, index);
+					item.ReadIndex(fileBytes, index);
+
+					DbxFolderIndex folderIndex = item.FolderIndex;
+
+					string message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						Name,
+						folderIndex.FolderName);
 					Log.Info(message);
 				}
 			}
