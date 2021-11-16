8f9869c07c2e485c668a4ea4d3d8947e013cb2b5
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-16 16:52:49 +0900

    Add List method

--- DbxToPstLibrary/DbxMessagesFile.cs
+++ DbxToPstLibrary/DbxMessagesFile.cs
@@ -29,6 +29,67 @@ namespace DbxToPstLibrary
 		}
 
 		/// <summary>
+		/// List messages method.
+		/// </summary>
+		public void List()
+		{
+			if (Tree != null)
+			{
+				byte[] fileBytes = GetFileBytes();
+
+				foreach (uint index in Tree.FolderInformationIndexes)
+				{
+					DbxMessageIndexedItem item = new();
+					item.ReadIndex(fileBytes, index);
+
+					DbxMessageIndex messageIndex = item.MessageIndex;
+
+					string message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.SenderName,
+						messageIndex.SenderName);
+					Log.Info(message);
+
+					message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.SenderEmailAddress,
+						messageIndex.SenderEmailAddress);
+					Log.Info(message);
+
+					message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.ReceivedTime,
+						messageIndex.ReceivedTime);
+					Log.Info(message);
+
+					message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.Subject,
+						messageIndex.Subject);
+					Log.Info(message);
+
+					message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.ReceiptentName,
+						messageIndex.ReceiptentName);
+					Log.Info(message);
+
+					message = string.Format(
+						CultureInfo.InvariantCulture,
+						"item value[{0}] is {1}",
+						DbxMessageIndexedItem.ReceiptentEmailAddress,
+						messageIndex.ReceiptentEmailAddress);
+					Log.Info(message);
+				}
+			}
+		}
+
+		/// <summary>
 		/// Migrate messages method.
 		/// </summary>
 		public void MigrateMessages()
