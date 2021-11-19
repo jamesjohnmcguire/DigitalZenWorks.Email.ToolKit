67b9699812a12b6bb0b449b7109fbdcee3ac98e1
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-19 14:25:43 +0900

    Add FolderMapper and MessageMapper classes

---
+++ DbxToPstLibrary/FolderMapper.cs
@@ -0,0 +1,28 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="FolderMapper.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DbxToPstLibrary
+{
+	/// <summary>
+	/// Folder mapper class.
+	/// </summary>
+	public class FolderMapper
+	{
+		/// <summary>
+		/// Gets or sets the dbx folder id.
+		/// </summary>
+		/// <value>The dbx folder id.</value>
+		public int DbxFolderId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the MAPI entry id.
+		/// </summary>
+		/// <value>The MAPI entry id.</value>
+		public string EntryId { get; set; }
+	}
+}
