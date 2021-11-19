4ca5ac7f0224997b5ec6169ac5dbff2dca77d0ab
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-19 14:44:15 +0900

    Add PstOutlook class

---
+++ DbxToPstLibrary/PstOutlook.cs
@@ -0,0 +1,17 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="PstOutlook.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DbxToPstLibrary
+{
+	/// <summary>
+	/// Provides support for interating with an Outlook PST file.
+	/// </summary>
+	public class PstOutlook
+	{
+	}
+}
