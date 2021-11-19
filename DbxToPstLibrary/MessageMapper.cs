776dd82e2ba1cdc17d5cf64781014ef8cfcecc67
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-19 14:25:43 +0900

    Add FolderMapper and MessageMapper classes

---
+++ DbxToPstLibrary/MessageMapper.cs
@@ -0,0 +1,17 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="MessageMapper.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+namespace DbxToPstLibrary
+{
+	/// <summary>
+	/// Message mapper class.
+	/// </summary>
+	public class MessageMapper
+	{
+	}
+}
