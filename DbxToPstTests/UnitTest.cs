84a00283379963a8b17cbbc250cd1491d85ade3e
JamesMc <jamesjohnmcguire@gmail.com>
2021-11-30 14:32:20 +0900

    Add TestCreatePstStore test method

--- DbxToPstTests/UnitTest.cs
+++ DbxToPstTests/UnitTest.cs
@@ -1,11 +1,20 @@
-using NUnit.Framework;
 using DbxToPstLibrary;
+using Microsoft.Office.Interop.Outlook;
+using NUnit.Framework;
 using System;
 
 namespace DbxToPstTests
 {
 	public class Tests
 	{
+		private static string applicationDataDirectory =
+			@"DigitalZenWorks\DbxToPst";
+		private static string baseDataDirectory =
+			Environment.GetFolderPath(
+				Environment.SpecialFolder.ApplicationData,
+				Environment.SpecialFolderOption.Create) + @"\" +
+				applicationDataDirectory;
+
 		[SetUp]
 		public void Setup()
 		{
@@ -18,14 +27,19 @@ namespace DbxToPstTests
 		}
 
 		[Test]
+		public void TestCreatePstStore()
+		{
+			string path = baseDataDirectory + "\\NewTest.pst";
+			PstOutlook pstOutlook = new PstOutlook();
+
+			Microsoft.Office.Interop.Outlook.Store store = pstOutlook.CreateStore(path);
+
+			Assert.NotNull(store);
+		}
+
+		[Test]
 		public void TestDbxToPst()
 		{
-			string applicationDataDirectory = @"DigitalZenWorks\DbxToPst";
-			string baseDataDirectory = Environment.GetFolderPath(
-				Environment.SpecialFolder.ApplicationData,
-				Environment.SpecialFolderOption.Create) + @"\" +
-				applicationDataDirectory;
-
 			string path = baseDataDirectory + "\\TestFolder";
 			bool result = Migrate.DbxToPst(path);
 			Assert.IsTrue(result);
