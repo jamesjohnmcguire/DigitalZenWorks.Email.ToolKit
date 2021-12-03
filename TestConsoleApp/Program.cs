2723e4b8891f4fb7d02175039a731f043569d638
JamesMc <jamesjohnmcguire@gmail.com>
2021-12-03 19:28:36 +0900

    Add TestConsoleApp

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Program.cs" company="James John McGuire">
+// Copyright © 2021 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using Common.Logging;
+using DbxToPstLibrary;
+using DigitalZenWorks.Email.DbxOutlookExpress;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+
+namespace DbxToPst.Test
+{
+	/// <summary>
+	/// Dbx to pst test program class.
+	/// </summary>
+	public static class Program
+	{
+		private static readonly ILog Log = LogManager.GetLogger(
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+			MethodBase.GetCurrentMethod().DeclaringType);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+		/// <summary>
+		/// The program's main entry point.
+		/// </summary>
+		/// <param name="arguments">The arguments given to the program.</param>
+		public static void Main(string[] arguments)
+		{
+			LogInitialization();
+
+			Log.Info("Test console app");
+
+			if (arguments != null && arguments.Length > 0)
+			{
+				DbxSet dbxSet = new DbxSet(arguments[0]);
+
+				dbxSet.List();
+			}
+			else
+			{
+				Log.Error("Invalid arguments");
+			}
+		}
+
+		private static void LogInitialization()
+		{
+			string applicationDataDirectory = @"DigitalZenWorks\DbxToPst";
+			string baseDataDirectory = Environment.GetFolderPath(
+				Environment.SpecialFolder.ApplicationData,
+				Environment.SpecialFolderOption.Create) + @"\" +
+				applicationDataDirectory;
+
+			string logFilePath = baseDataDirectory + "\\DbxToPst.log";
+			string outputTemplate =
+				"[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] " +
+				"{Message:lj}{NewLine}{Exception}";
+
+			LoggerConfiguration configuration = new ();
+			LoggerSinkConfiguration sinkConfiguration = configuration.WriteTo;
+			sinkConfiguration.Console(LogEventLevel.Verbose, outputTemplate);
+			sinkConfiguration.File(
+				logFilePath, LogEventLevel.Verbose, outputTemplate);
+			Serilog.Log.Logger = configuration.CreateLogger();
+
+			LogManager.Adapter =
+				new Common.Logging.Serilog.SerilogFactoryAdapter();
+		}
+	}
+}
