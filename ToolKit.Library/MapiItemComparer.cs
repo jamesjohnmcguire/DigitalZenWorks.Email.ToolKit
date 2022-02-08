086ff04d6959504fc8890c7aec19a791462a488b
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-08 18:55:43 +0900

    Add GetRecipients method in compare

---
+++ ToolKit.Library/MapiItemComparer.cs
@@ -0,0 +1,90 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="MapiItemComparer.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+
+using Common.Logging;
+using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit.Library
+{
+	/// <summary>
+	/// Provides comparision support for Outlook MAPI items.
+	/// </summary>
+	public class MapiItemComparer
+	{
+		private static readonly ILog Log = LogManager.GetLogger(
+			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage(
+			"StyleCop.CSharp.NamingRules",
+			"SA1305:Field names should not use Hungarian notation",
+			Justification = "It isn't hungarian notation.")]
+		private static string GetRecipients (MailItem mailItem)
+		{
+			string recipients = string.Empty;
+			List<string> toList = new List<string> ();
+			List<string> ccList = new List<string>();
+			List<string> bccList = new List<string>();
+
+			foreach (Recipient recipient in mailItem.Recipients)
+			{
+				string formattedRecipient = string.Format(
+					CultureInfo.InvariantCulture,
+					"{0} <{1}>; ",
+					recipient.Name,
+					recipient.Address);
+
+				OlMailRecipientType recipientType =
+					(OlMailRecipientType)recipient.Type;
+				switch (recipientType)
+				{
+					case OlMailRecipientType.olTo:
+						toList.Add(formattedRecipient);
+						break;
+					case OlMailRecipientType.olCC:
+						ccList.Add(formattedRecipient);
+						break;
+					case OlMailRecipientType.olBCC:
+						bccList.Add(formattedRecipient);
+						break;
+					case OlMailRecipientType.olOriginator:
+						Log.Warn("Ignoring olOriginator recipient type");
+						break;
+					default:
+						Log.Warn("Ignoring uknown recipient type");
+						break;
+				}
+			}
+
+			toList.Sort();
+			ccList.Sort();
+			bccList.Sort();
+
+			foreach (string formattedRecipient in toList)
+			{
+				recipients += formattedRecipient;
+			}
+
+			foreach (string formattedRecipient in ccList)
+			{
+				recipients += formattedRecipient;
+			}
+
+			foreach (string formattedRecipient in bccList)
+			{
+				recipients += formattedRecipient;
+			}
+
+			return recipients;
+		}
+	}
+}
