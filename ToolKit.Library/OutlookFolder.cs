2861ba604abf3c96c5425168bc7b67a8bb8e8869
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-01 22:52:46 +0900

    Add OutlookFolder class

---
+++ ToolKit.Library/OutlookFolder.cs
@@ -0,0 +1,21 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="OutlookFolder.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolKit.Library
+{
+	/// <summary>
+	/// Represents an Outlook Folder.
+	/// </summary>
+	internal class OutlookFolder
+	{
+	}
+}
