b8c939a1df89701ef903bf930c45148d40a1825c
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-02 18:45:31 +0900

    Add OutlookAccount class

---
+++ ToolKit.Library/OutlookAccount.cs
@@ -0,0 +1,15 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="OutlookAccount.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Email.ToolKit
+{
+	/// <summary>
+	/// Represents an Outlook account.
+	/// </summary>
+	public class OutlookAccount
+	{
+	}
+}
