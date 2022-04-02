a2584fea5bd1db031f5e9304115cb0ac54992f9e
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-02 18:50:43 +0900

    Add outlook application and session

--- ToolKit.Library/OutlookAccount.cs
+++ ToolKit.Library/OutlookAccount.cs
@@ -4,6 +4,8 @@
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
+using Microsoft.Office.Interop.Outlook;
+
 namespace DigitalZenWorks.Email.ToolKit
 {
 	/// <summary>
@@ -11,5 +13,24 @@ namespace DigitalZenWorks.Email.ToolKit
 	/// </summary>
 	public class OutlookAccount
 	{
+		private readonly Application application;
+		private readonly NameSpace session;
+
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="OutlookAccount"/> class.
+		/// </summary>
+		public OutlookAccount()
+		{
+			application = new ();
+
+			session = application.Session;
+		}
+
+		/// <summary>
+		/// Gets the default session (Outlook namespace).
+		/// </summary>
+		/// <value>The default session (Outlook namespace).</value>
+		public NameSpace Session { get { return session; } }
 	}
 }
