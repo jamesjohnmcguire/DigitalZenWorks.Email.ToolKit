effa648d93511d7b87e89b8946ca8971d203d172
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-07 00:38:23 +0900

    Add publish command to build

--- DevelopmentTools/build.cmd
+++ DevelopmentTools/build.cmd
@@ -1,14 +1,29 @@
+REM %1 - Type of build
+REM %2 - API key
+REM %3 - Version (such as 1.0.0.5)
+
 CD %~dp0
 CD ..
 
+IF "%1"=="publish" GOTO release
 IF "%1"=="release" GOTO release
 
+:default
 msbuild -property:Configuration=Release;IncludeAllContentForSelfExtract=true;Platform="Any CPU";PublishReadyToRun=true;PublishSingleFile=true;Runtimeidentifier=win-x64;SelfContained=true;TargetFramework=net6.0-windows -restore -target:publish;rebuild ToolKit.Application
 
 GOTO end
 
+:publish
+
+msbuild -property:Configuration=Release;OutputPath=Bin\Publish -restore -target:rebuild ToolKit.Library
+msbuild -property:Configuration=Release;OutputPath=Bin\Publish -target:pack ToolKit.Library
+
+cd ToolKit.Library\Bin\Publish
+
+nuget push DigitalZenWorks.Email.ToolKit.%2.nupkg %3
+GOTO end
+
 :release
-IF EXIST Bin\Release\AnyCPU\NUL DEL /Q Bin\Release\AnyCPU\*.*
 
 CALL msbuild -property:Configuration=Release;IncludeAllContentForSelfExtract=true;Platform="Any CPU";PublishReadyToRun=true;PublishSingleFile=true;Runtimeidentifier=win-x64;SelfContained=true;TargetFramework=net6.0-windows -restore -target:publish;rebuild ToolKit.Application
 CD Bin\Release\AnyCpu\win-x64\publish
