df2faecabb124faa2c188f78a4fa25fabf97ec2d
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-22 19:42:51 +0900

    Add default info methods

--- ToolKit.Library/LogFormatMessage.cs
+++ ToolKit.Library/LogFormatMessage.cs
@@ -1,10 +1,12 @@
 ﻿/////////////////////////////////////////////////////////////////////////////
-// <copyright file="Migrate.cs" company="James John McGuire">
+// <copyright file="LogFormatMessage.cs" company="James John McGuire">
 // Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
+using Common.Logging;
 using System;
+using System.Globalization;
 using System.Text;
 
 namespace ToolKit.Library
@@ -15,5 +17,48 @@ namespace ToolKit.Library
 	/// </summary>
 	public static class LogFormatMessage
 	{
+		private static readonly ILog Log = LogManager.GetLogger(
+			System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		/// <summary>
+		/// Sends an info log message.
+		/// </summary>
+		/// <param name="template">The format template to use.</param>
+		/// <param name="parameter1">The first parameter.</param>
+		/// <param name="parameter2">The second parameter.</param>
+		public static void Info(
+			string template, string parameter1, string parameter2)
+		{
+			string message = string.Format(
+				CultureInfo.InvariantCulture,
+				template,
+				parameter1,
+				parameter2);
+
+			Log.Info(message);
+		}
+
+		/// <summary>
+		/// Sends an info log message.
+		/// </summary>
+		/// <param name="template">The format template to use.</param>
+		/// <param name="parameter1">The first parameter.</param>
+		/// <param name="parameter2">The second parameter.</param>
+		/// <param name="parameter3">The third parameter.</param>
+		public static void Info(
+			string template,
+			string parameter1,
+			string parameter2,
+			string parameter3)
+		{
+			string message = string.Format(
+				CultureInfo.InvariantCulture,
+				template,
+				parameter1,
+				parameter2,
+				parameter3);
+
+			Log.Info(message);
+		}
 	}
 }
