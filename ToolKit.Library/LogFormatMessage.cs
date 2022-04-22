7e10fadbc4e0c4ca2070398eec433dd736da3ef4
James John McGuire <jamesjohnmcguire@gmail.com>
2022-04-22 19:32:41 +0900

    Add new class to encapsulate formatted log messages

---
+++ ToolKit.Library/LogFormatMessage.cs
@@ -0,0 +1,19 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Migrate.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text;
+
+namespace ToolKit.Library
+{
+	/// <summary>
+	/// Provides support for compiling a formatted message and
+	/// logging that message.
+	/// </summary>
+	public static class LogFormatMessage
+	{
+	}
+}
