eaf460812319c36cf9216e3c3967b1e45aafc366
James John McGuire <jamesjohnmcguire@gmail.com>
2022-05-03 21:05:55 +0900

    Add Trim method to HtmlEmail

--- ToolKit.Library/HtmlEmail.cs
+++ ToolKit.Library/HtmlEmail.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace DigitalZenWorks.Email.ToolKit
@@ -15,7 +16,27 @@ namespace DigitalZenWorks.Email.ToolKit
 	/// <summary>
 	/// Represents a HTML email body.
 	/// </summary>
-	public class HtmlEmail
+	public static class HtmlEmail
 	{
+		/// <summary>
+		/// Trim the end of the HTML body.
+		/// </summary>
+		/// <param name="htmlBody">The HTML body to trim.</param>
+		/// <returns>The trimmed HTML body.</returns>
+		public static string Trim(string htmlBody)
+		{
+			string pattern = @"(\\r\\n)+(?=\\r\\n</BODY>\\r\\n</HTML>$)";
+
+			htmlBody = Regex.Replace(
+				htmlBody, pattern, string.Empty, RegexOptions.ExplicitCapture);
+
+			pattern = @"(<BR>\\r\\n)+(?=<BR>\\r\\n</FONT>\\r\\n" +
+				@"</P>\\r\\n</BODY>\\r\\n</HTML>$)";
+
+			htmlBody = Regex.Replace(
+				htmlBody, pattern, string.Empty, RegexOptions.ExplicitCapture);
+
+			return htmlBody;
+		}
 	}
 }
