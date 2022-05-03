e98110b7d55f70243bc2dcebfd33e1067042a017
James John McGuire <jamesjohnmcguire@gmail.com>
2022-05-03 17:58:57 +0900

    Add HtmlEmail class

---
+++ ToolKit.Library/HtmlEmail.cs
@@ -0,0 +1,21 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="HtmlEmail.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalZenWorks.Email.ToolKit
+{
+	/// <summary>
+	/// Represents a HTML email body.
+	/// </summary>
+	public class HtmlEmail
+	{
+	}
+}
