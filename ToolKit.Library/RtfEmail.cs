9f077077a90091258bb8ff8fa206e027a4d76512
James John McGuire <jamesjohnmcguire@gmail.com>
2022-05-03 21:19:40 +0900

    Add RtfEmail class

---
+++ ToolKit.Library/RtfEmail.cs
@@ -0,0 +1,27 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="RtfEmail.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace DigitalZenWorks.Email.ToolKit
+{
+	/// <summary>
+	/// Represents a RTF email body.
+	/// </summary>
+	public static class RtfEmail
+	{
+		/// <summary>
+		/// Trim the end of the RTF body.
+		/// </summary>
+		/// <param name="rtfBody">The RTF body to trim.</param>
+		/// <returns>The trimmed RTF body.</returns>
+		public static string Trim(string rtfBody)
+		{
+			return rtfBody;
+		}
+	}
+}
