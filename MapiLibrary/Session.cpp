a9914d4b8bf73edd95afd305dac1f57e8144c370
James John McGuire <jamesjohnmcguire@gmail.com>
2022-06-28 19:12:31 +0900

    Add calls to initialize and login

--- MapiLibrary/Session.cpp
+++ MapiLibrary/Session.cpp
@@ -5,5 +5,17 @@ namespace MapiLibrary
 {
 	Session::Session()
 	{
+		HRESULT result = MAPIInitialize(&MAPIINIT);
+
+		if (result == S_OK)
+		{
+			result = MAPILogonEx(
+				0,
+				nullptr,
+				nullptr,
+				logonFlags,
+				&mapiSession
+			);
+		}
 	}
 }
