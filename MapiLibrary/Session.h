977af15974eba6e7a253d7bad5c7988569baf2a8
James John McGuire <jamesjohnmcguire@gmail.com>
2022-07-04 20:05:01 +0900

    Add method GetStores

--- MapiLibrary/Session.h
+++ MapiLibrary/Session.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <vector>
 
 #include "MapiLibrary.h"
+#include "Store.h"
 
 namespace MapiLibrary
 {
@@ -10,13 +12,15 @@ namespace MapiLibrary
 	{
 		public:
 			DllExport Session();
-			~Session();
+			DllExport ~Session();
 
 			DllExport void Close();
+			DllExport std::vector<Store*> GetStores();
 
 		private:
 			ULONG logonFlags = MAPI_ALLOW_OTHERS | MAPI_EXTENDED |
 				MAPI_NO_MAIL | MAPI_USE_DEFAULT | MAPI_UNICODE;
 			LPMAPISESSION mapiSession{};
+			std::vector<Store*> stores;
 	};
 }
