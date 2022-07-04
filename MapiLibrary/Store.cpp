e546d43f0ada96598851f23c1242163d25b3880e
James John McGuire <jamesjohnmcguire@gmail.com>
2022-07-04 20:04:35 +0900

    Fill out Store constructor

--- MapiLibrary/Store.cpp
+++ MapiLibrary/Store.cpp
@@ -1,2 +1,14 @@
 #include "pch.h"
 #include "Store.h"
+
+namespace MapiLibrary
+{
+	Store::Store(ULONG entryIdLengthIn, LPENTRYID entryIdIn)
+		: entryIdLength(entryIdLengthIn), entryId(entryIdIn)
+	{
+	}
+
+	Store::~Store()
+	{
+	}
+}
