13e1bee0357922c39737f7579d8d5b1bdd3ddf72
James John McGuire <jamesjohnmcguire@gmail.com>
2022-07-06 17:02:59 +0900

    Add DeclarationsSpecifications.h and move defines

---
+++ MapiLibrary/DeclarationsSpecifications.h
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace MapiLibrary
+{
+	#if defined _WIN32 || defined __CYGWIN__
+		#ifdef DLL_EXPORTS
+			#if defined WIN32
+				#define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
+				#define DllExport __declspec(dllexport)
+			#else
+				#define LIB_API(RetType) extern "C" RetType __attribute__((visibility("default")))
+			#endif
+
+			#ifdef __GNUC__
+				#define EXPORT_API extern "C" __attribute__ ((dllexport))
+			#else
+				// Note: actually gcc seems to also supports this syntax.
+				#define EXPORT_API extern "C" __declspec (dllexport)
+			#endif
+		#else
+			#define DllExport
+
+			#if defined WIN32
+				#define LIB_API(RetType) extern "C" __declspec(dllimport) RetType
+			#else
+				#define LIB_API(RetType) extern "C" RetType
+			#endif
+
+			#ifdef __GNUC__
+				#define EXPORT_API extern "C" __attribute__ ((dllimport))
+			#else
+				#define EXPORT_API extern "C" __declspec (dllimport)
+			#endif
+		#endif
+	#else
+		#if __GNUC__ >= 4
+			#define EXPORT_API extern "C" __attribute__ ((visibility ("default")))
+			#define LIB_API(RetType) RetType
+		#else
+			#define EXPORT_API
+			#define LIB_API(RetType) RetType
+		#endif
+	#endif
+}
