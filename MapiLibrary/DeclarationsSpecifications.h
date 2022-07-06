df67d159cb4f6899bbf167d2e2943c36b671acde
James John McGuire <jamesjohnmcguire@gmail.com>
2022-07-07 00:46:10 +0900

    Use project unique define

--- MapiLibrary/DeclarationsSpecifications.h
+++ MapiLibrary/DeclarationsSpecifications.h
@@ -3,7 +3,7 @@
 namespace MapiLibrary
 {
 	#if defined _WIN32 || defined __CYGWIN__
-		#ifdef DLL_EXPORTS
+		#ifdef MAPILIBRARY_EXPORTS
 			#if defined WIN32
 				#define LIB_API(RetType) extern "C" __declspec(dllexport) RetType
 				#define DllExport __declspec(dllexport)
