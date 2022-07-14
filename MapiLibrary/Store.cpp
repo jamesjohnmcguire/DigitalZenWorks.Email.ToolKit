86bfc5289027ba7993320b9e3be6400b19034c59
James John McGuire <jamesjohnmcguire@gmail.com>
2022-07-14 19:43:31 +0900

    Open store and get root folder

--- MapiLibrary/Store.cpp
+++ MapiLibrary/Store.cpp
@@ -3,12 +3,44 @@
 
 namespace MapiLibrary
 {
-	Store::Store(ULONG entryIdLengthIn, LPENTRYID entryIdIn)
-		: entryIdLength(entryIdLengthIn), entryId(entryIdIn)
+	Store::Store(
+		LPMAPISESSION mapiSessionIn, ULONG entryIdLengthIn, LPENTRYID entryIdIn)
+		:
+			mapiSession(mapiSessionIn),
+			entryIdLength(entryIdLengthIn),
+			entryId(entryIdIn)
 	{
 	}
 
 	Store::~Store()
 	{
+	}
+
+	int Store::RemoveDuplicates()
+	{
+		int duplicatesRemoved = 0;
+
+		HRESULT result = mapiSession->OpenMsgStore(
+			0L,
+			entryIdLength,
+			entryId,
+			nullptr,
+			MAPI_BEST_ACCESS,
+			&mapiDatabase);
+
+		if (result == S_OK)
+		{
+			ULONG objectType = 0;
+
+			result = mapiDatabase->OpenEntry(
+				0,
+				nullptr,
+				nullptr,
+				MAPI_MODIFY | MAPI_DEFERRED_ERRORS,
+				&objectType,
+				(LPUNKNOWN*)&rootFolder);
+		}
+
+		return duplicatesRemoved;
 	}
 }
