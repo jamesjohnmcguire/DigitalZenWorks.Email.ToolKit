523d869c79de3c4e3c0d8adcdfff1c64aad6279e
James John McGuire <jamesjohnmcguire@gmail.com>
2022-08-04 20:51:47 +0900

    Add MapiProperties for custom defines

---
+++ MapiLibrary/MapiProperties.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/////////////////////////////////////////////////////////////////////////////
+// Defines
+/////////////////////////////////////////////////////////////////////////////
+#ifndef PR_BODY_HTML
+	#define PR_BODY_HTML	0x1013001F
+#endif
+#ifndef PR_CONVERSATION_TOPIC
+	#define PR_CONVERSATION_TOPIC	PROP_TAG(PT_UNICODE, 0x0070)
+#endif
+#ifndef PR_HTML
+	#define PR_HTML 0x10130102
+#endif
+#ifndef PR_INTERNET_MESSAGE_ID
+	#define PR_INTERNET_MESSAGE_ID	PROP_TAG(PT_UNICODE, 0x1035)
+#endif
+#ifndef STORE_HTML_OK
+	#define STORE_HTML_OK	0x00010000
+#endif
+#ifndef PR_INTERNET_CPID
+	#define PR_INTERNET_CPID 0x3FDE0003
+#endif
