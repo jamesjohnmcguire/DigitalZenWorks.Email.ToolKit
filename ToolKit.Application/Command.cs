0064ec7144c69b42cd3b85407ba480c6ffe92636
James John McGuire <jamesjohnmcguire@gmail.com>
2022-10-21 17:51:57 +0900

    Refactor Constuctors, Add description

--- ToolKit.Application/Command.cs
+++ ToolKit.Application/Command.cs
@@ -7,18 +7,33 @@
 using System;
 using System.Collections.Generic;
 
-namespace ToolKit.Application
+namespace DigitalZenWorks.Email.ToolKit.Application
 {
 	/// <summary>
 	/// Represents a command line command.
 	/// </summary>
 	public class Command
 	{
-		private readonly string description;
 		private readonly string name;
 		private readonly IList<CommandOption> options;
 		private readonly int parameterCount;
+
+		private string description;
 		private IList<string> parameters = new List<string>();
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Command"/> class.
+		/// </summary>
+		/// <param name="name">The command name.</param>
+		public Command(string name)
+		{
+			this.name = name;
+
+			if (options == null)
+			{
+				options = new List<CommandOption>();
+			}
+		}
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="Command"/> class.
@@ -26,12 +41,27 @@ namespace ToolKit.Application
 		/// <param name="name">The command name.</param>
 		/// <param name="options">The command options.</param>
 		/// <param name="parameterCount">The command parameter count.</param>
+		/// <param name="description">The command description.</param>
 		public Command(
-			string name, IList<CommandOption> options, int parameterCount)
+			string name,
+			IList<CommandOption> options,
+			int parameterCount,
+			string description)
+			: this(name)
 		{
-			this.name = name;
 			this.options = options;
 			this.parameterCount = parameterCount;
+			this.description = description;
+		}
+
+		/// <summary>
+		/// Gets or sets the command description.
+		/// </summary>
+		/// <value>The command description.</value>
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
 		}
 
 		/// <summary>
