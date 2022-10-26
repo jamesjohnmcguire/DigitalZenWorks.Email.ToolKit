1aa5d46e5c536f63b7ee4daa6331e2d0e0ce58cb
James John McGuire <jamesjohnmcguire@gmail.com>
2022-10-26 20:34:40 +0900

    Add Test Project for New Project

---
+++ CommandLineCommands.Tests/CommandLineTests.cs
@@ -0,0 +1,27 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="CommandLineTests.cs" company="James John McGuire">
+// Copyright © 2021 - 2022 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using NUnit.Framework;
+using System;
+
+[assembly: CLSCompliant(true)]
+
+namespace CommandLineCommands.Tests
+{
+	public class CommandLineTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
