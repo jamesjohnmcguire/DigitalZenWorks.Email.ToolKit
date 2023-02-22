5de2b04fe633bedc7bb03c783fb32aa8cfc785ba
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-22 22:42:36 +0900

    Add Beginnings of Mapi Tests

---
+++ MapiLibraryTests/test.cpp
@@ -0,0 +1,39 @@
+#include "pch.h"
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include "../MapiLibrary/Session.h"
+#include "../MapiLibrary/Store.h"
+
+using namespace MapiLibrary;
+
+TEST(TestSanityCheck, SanityCheck)
+{
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
+
+TEST(TestGetMessage, GetMessage)
+{
+	Session* session = new Session();
+
+	std::vector<std::shared_ptr<Store>> stores = session->GetStores();
+
+	size_t count = stores.size();
+
+	for (size_t index = 0; index < count; index++)
+	{
+		std::shared_ptr<Store> storePointer = stores[index];
+		Store* store = storePointer.get();
+
+		store->RemoveDuplicates();
+	}
+
+	session->Close();
+	session = nullptr;
+
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
