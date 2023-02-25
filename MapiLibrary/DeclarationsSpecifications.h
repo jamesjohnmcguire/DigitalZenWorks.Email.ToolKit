21edad33ea17d78785dca0880110615fc1ac6aa1
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-25 18:18:20 +0900

    Enhance API Declaration Specifiers

--- MapiLibrary/DeclarationsSpecifications.h
+++ MapiLibrary/DeclarationsSpecifications.h
@@ -3,10 +3,24 @@
 namespace MapiLibrary
 {
 	#ifdef MAPILIBRARY_EXPORTS
-		#define API extern "C" __declspec(dllexport)
+		#define API(RetType) extern "C" __declspec(dllexport) RetType
 		#define DllExport __declspec(dllexport)
+
+		#ifdef __GNUC__
+			#define EXPORT_API extern "C" __attribute__ ((dllexport))
+		#else
+			// Note: actually gcc seems to also supports this syntax.
+			#define EXPORT_API extern "C" __declspec (dllexport)
+		#endif
 	#else
-		#define API extern "C" __declspec(dllimport)
 		#define DllExport
+
+		#define API(RetType) extern "C" __declspec(dllimport) RetType
+
+		#ifdef __GNUC__
+			#define EXPORT_API extern "C" __attribute__ ((dllimport))
+		#else
+			#define EXPORT_API extern "C" __declspec (dllimport)
+		#endif
 	#endif
 }
