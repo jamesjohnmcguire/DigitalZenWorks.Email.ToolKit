aaec8077b0ebf584c80151f8077b21668fdd1335
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-27 20:30:51 +0900

    Continuation of Hashing of Mapi Properties

--- MapiLibrary/Message.h
+++ MapiLibrary/Message.h
@@ -7,14 +7,17 @@ namespace MapiLibrary
 {
 	class Message
 	{
-		public:
-			Message(LPMESSAGE messageIn);
-			Message(LPMESSAGE messageIn, std::string applicationName);
-			std::string GetMessageHash();
+	public:
+		Message(LPMESSAGE messageIn);
+		Message(LPMESSAGE messageIn, std::string applicationName);
+		std::string GetMessageHash();
 
-		private:
-			std::string applicationName;
-			std::shared_ptr<spdlog::logger> logger = nullptr;
-			LPMESSAGE message;
+	private:
+		std::string applicationName;
+		std::shared_ptr<spdlog::logger> logger = nullptr;
+		LPMESSAGE message;
+
+		std::vector<byte> GetBytes(std::string text);
+		std::string GetStringProperty(SPropValue property);
 	};
 }
