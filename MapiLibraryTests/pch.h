43af2ee3836fe9e571f8dd9a0a1d5670b15644e7
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-27 20:32:18 +0900

    Add Explicit References to Required Headers

--- MapiLibraryTests/pch.h
+++ MapiLibraryTests/pch.h
@@ -1,7 +1,17 @@
-//
-// pch.h
-//
-
 #pragma once
+
+#include <MAPIDefS.h>
+#include <EdkMdb.h>
+#include <MAPITags.h>
+#include <MAPIUtil.h>
+#include <MAPIX.h>
+
+#pragma warning(push)
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/stdout_sinks.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#pragma warning(pop)
 
 #include "gtest/gtest.h"
