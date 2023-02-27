515a069554cfc39369db6887831f1cffbd88d5af
James John McGuire <jamesjohnmcguire@gmail.com>
2023-02-27 20:29:01 +0900

    Add DllExport Specifiers

--- MapiLibrary/UnicodeText.h
+++ MapiLibrary/UnicodeText.h
@@ -7,10 +7,12 @@ namespace MapiLibrary
 	class UnicodeText
 	{
 		public:
-			static std::string GetUtf8Text(const std::wstring& wideString);
-			static std::wstring GetWideText(const std::string& utf8Text);
+			DllExport static char* GetUtf8Text(const wchar_t* wideString);
+			DllExport static std::string GetUtf8Text(
+				const std::wstring& wideString);
+			DllExport static std::wstring GetWideText(
+				const std::string& utf8Text);
 
-			static char* GetUtf8Text(const wchar_t* wideString);
 			DllExport static void SetConsole();
 	};
 }
