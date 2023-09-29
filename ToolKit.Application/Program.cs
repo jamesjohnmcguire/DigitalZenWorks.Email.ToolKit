6b819d09ab6aeda330407133e03ef3bf66d5c34b
James John McGuire <jamesjohnmcguire@gmail.com>
2023-09-29 16:05:17 +0900

    Add Culture Provider to Remove Warning

--- ToolKit.Application/Program.cs
+++ ToolKit.Application/Program.cs
@@ -617,9 +617,15 @@ namespace DigitalZenWorks.Email.ToolKit.Application
 
 			LoggerConfiguration configuration = new ();
 			LoggerSinkConfiguration sinkConfiguration = configuration.WriteTo;
-			sinkConfiguration.Console(LogEventLevel.Verbose, outputTemplate);
+			sinkConfiguration.Console(
+				LogEventLevel.Verbose,
+				outputTemplate,
+				CultureInfo.InvariantCulture);
 			sinkConfiguration.File(
-				logFilePath, LogEventLevel.Verbose, outputTemplate);
+				logFilePath,
+				LogEventLevel.Verbose,
+				outputTemplate,
+				CultureInfo.InvariantCulture);
 			Serilog.Log.Logger = configuration.CreateLogger();
 
 			LogManager.Adapter =
