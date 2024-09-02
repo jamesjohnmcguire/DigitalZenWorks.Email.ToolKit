3c1f83db6719bf9d7f82ba66a322ca7ceb07898e
James John McGuire <jamesjohnmcguire@gmail.com>
2024-09-02 11:54:24 +0900

    Add IContentItem Interface

---
+++ ToolKit.Library/IContentItem.cs
@@ -0,0 +1,39 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="IContentItem.cs" company="James John McGuire">
+// Copyright © 2021 - 2024 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+using Microsoft.Office.Interop.Outlook;
+
+namespace ToolKit.Library
+{
+	/// <summary>
+	/// Content Item Interface.
+	/// </summary>
+	public interface IContentItem
+	{
+		/// <summary>
+		/// Gets the item's hash text.
+		/// </summary>
+		/// <value>The item's hash stext.</value>
+		string Hash { get; }
+
+		/// <summary>
+		/// Gets the item's synopses text.
+		/// </summary>
+		/// <value>The item's synopses text.</value>
+		string Synopses { get; }
+
+		/// <summary>
+		/// Deletes the given item.
+		/// </summary>
+		public void Delete();
+
+		/// <summary>
+		/// Moves the given item.
+		/// </summary>
+		/// <param name="destination">The destination folder.</param>
+		public void Move(MAPIFolder destination);
+	}
+}
