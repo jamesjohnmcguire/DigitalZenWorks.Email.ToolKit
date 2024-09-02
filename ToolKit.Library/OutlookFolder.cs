533b417781f01c036e889350ea5bfd53275ed621
James John McGuire <jamesjohnmcguire@gmail.com>
2024-09-02 21:34:29 +0900

    Remove Unused Usings

--- ToolKit.Library/OutlookFolder.cs
+++ ToolKit.Library/OutlookFolder.cs
@@ -11,7 +11,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
