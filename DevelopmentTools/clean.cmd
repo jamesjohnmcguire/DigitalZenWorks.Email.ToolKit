a0af88450c22579a4f795938ff1605e596704fb1
James John McGuire <jamesjohnmcguire@gmail.com>
2026-04-09 22:54:05 +0900

    Fill Out Clean Script

--- DevelopmentTools/clean.cmd
+++ DevelopmentTools/clean.cmd
@@ -2,25 +2,32 @@
 CD %~dp0
 CD ..
 
-RD /S /Q .vs
-RD /S /Q 1
-RD /S /Q Bin
-RD /S /Q TestConsoleApp\obj
-RD /S /Q ToolKit.Application\Bin
-RD /S /Q ToolKit.Application\obj
-RD /S /Q ToolKit.Library\Bin
-RD /S /Q ToolKit.Library\obj
-RD /S /Q ToolKit.Tests\bin
-RD /S /Q ToolKit.Tests\obj
-RD /S /Q CommandLineCommands\CommandLineCommands.Tests\obj
-RD /S /Q CommandLineCommands\CommandLineCommands\obj
-RD /S /Q DbxOutlookExpress\DbxOutlookExpress\1
-RD /S /Q DbxOutlookExpress\DbxOutlookExpress\obj
-RD /S /Q DbxOutlookExpress\DbxOutlookExpressTests\bin
-RD /S /Q DbxOutlookExpress\DbxOutlookExpressTests\obj
-RD /S /Q MsgKit\MsgKit\Bin
-RD /S /Q MsgKit\MsgKit\obj
-RD /S /Q UtilitiesNet\UtilitiesNetLibrary\bin
-RD /S /Q UtilitiesNet\UtilitiesNetLibrary\obj
-RD /S /Q UtilitiesNet\UtilitiesNetTests\bin
-RD /S /Q UtilitiesNet\UtilitiesNetTests\obj
+DEL /S /Q *.csproj.user
+
+IF EXIST .vs\NUL RD /S /Q .vs
+IF EXIST 1\NUL RD /S /Q 1
+IF EXIST Bin\NUL RD /S /Q Bin
+
+IF EXIST TestConsoleApp\bin\NUL RD /S /Q TestConsoleApp\bin
+IF EXIST TestConsoleApp\obj\NUL RD /S /Q TestConsoleApp\obj
+IF EXIST ToolKit.Application\Bin\NUL RD /S /Q ToolKit.Application\Bin
+IF EXIST ToolKit.Application\obj\NUL RD /S /Q ToolKit.Application\obj
+IF EXIST ToolKit.Library\Bin\NUL RD /S /Q ToolKit.Library\Bin
+IF EXIST ToolKit.Library\obj\NUL RD /S /Q ToolKit.Library\obj
+IF EXIST ToolKit.Tests\bin\NUL RD /S /Q ToolKit.Tests\bin
+IF EXIST ToolKit.Tests\obj\NUL RD /S /Q ToolKit.Tests\obj
+IF EXIST CommandLineCommands\CommandLineCommands.Tests\bin\NUL RD /S /Q CommandLineCommands\CommandLineCommands.Tests\bin
+IF EXIST CommandLineCommands\CommandLineCommands.Tests\obj\NUL RD /S /Q CommandLineCommands\CommandLineCommands.Tests\obj
+IF EXIST CommandLineCommands\CommandLineCommands\bin\NUL RD /S /Q CommandLineCommands\CommandLineCommands\bin
+IF EXIST CommandLineCommands\CommandLineCommands\obj\NUL RD /S /Q CommandLineCommands\CommandLineCommands\obj
+IF EXIST DbxOutlookExpress\DbxOutlookExpress\1\NUL RD /S /Q DbxOutlookExpress\DbxOutlookExpress\1
+IF EXIST DbxOutlookExpress\DbxOutlookExpress\bin\NUL RD /S /Q DbxOutlookExpress\DbxOutlookExpress\bin
+IF EXIST DbxOutlookExpress\DbxOutlookExpress\obj\NUL RD /S /Q DbxOutlookExpress\DbxOutlookExpress\obj
+IF EXIST DbxOutlookExpress\DbxOutlookExpressTests\bin\NUL RD /S /Q DbxOutlookExpress\DbxOutlookExpressTests\bin
+IF EXIST DbxOutlookExpress\DbxOutlookExpressTests\obj\NUL RD /S /Q DbxOutlookExpress\DbxOutlookExpressTests\obj
+IF EXIST MsgKit\MsgKit\Bin\NUL RD /S /Q MsgKit\MsgKit\Bin
+IF EXIST MsgKit\MsgKit\obj\NUL RD /S /Q MsgKit\MsgKit\obj
+IF EXIST UtilitiesNet\UtilitiesNetLibrary\bin\NUL RD /S /Q UtilitiesNet\UtilitiesNetLibrary\bin
+IF EXIST UtilitiesNet\UtilitiesNetLibrary\obj\NUL RD /S /Q UtilitiesNet\UtilitiesNetLibrary\obj
+IF EXIST UtilitiesNet\UtilitiesNetTests\bin\NUL RD /S /Q UtilitiesNet\UtilitiesNetTests\bin
+IF EXIST UtilitiesNet\UtilitiesNetTests\obj\NUL RD /S /Q UtilitiesNet\UtilitiesNetTests\obj
