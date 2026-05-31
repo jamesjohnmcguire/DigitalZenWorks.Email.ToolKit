d8d666f23c16f03180c24e23ddc677614cfb5d3d
JamesMc <jamesjohnmcguire@gmail.com>
2026-06-01 02:26:05 +0900

    Add IsOutlookInstalled Method

--- SourceCode/ToolKit.Library/OutlookService.cs
+++ SourceCode/ToolKit.Library/OutlookService.cs
@@ -1,15 +1,47 @@
-﻿/////////////////////////////////////////////////////////////////////////////
-// <copyright file="IContentItem.cs" company="James John McGuire">
+/////////////////////////////////////////////////////////////////////////////
+// <copyright file="OutlookService.cs" company="James John McGuire">
 // Copyright © 2021 - 2026 James John McGuire. All Rights Reserved.
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
+namespace DigitalZenWorks.Email.ToolKit;
+
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Microsoft.Win32;
+using Outlook = Microsoft.Office.Interop.Outlook;
 
-namespace DigitalZenWorks.Email.ToolKit;
-
-internal class OutlookService : IOutlookService
+public static class OutlookService : IOutlookService
 {
+	public static bool IsOutlookInstalled()
+	{
+		bool installed = false;
+
+		string registryPath =
+			@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\OUTLOOK.EXE";
+		using RegistryKey? key =
+			Registry.LocalMachine.OpenSubKey(registryPath);
+
+		if (key != null)
+		{
+			installed = true;
+		}
+		else
+		{
+			// 32-bit Outlook on 64-bit Windows
+			string registryPath = @"SOFTWARE\WOW6432Node\Microsoft\Windows\" +
+				@"CurrentVersion\App Paths\OUTLOOK.EXE"
+			using RegistryKey? wowKey =
+				Registry.LocalMachine.OpenSubKey(registryPath);
+
+			if (wowKey != null)
+			{
+				installed = true;
+			}
+		}
+	
+		return installed;
+	}
 }
